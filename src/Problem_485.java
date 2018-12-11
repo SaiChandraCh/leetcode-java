eb1b6cc72cc6bc9f79e1260e8ddd2ef986c9a1ed
SaiChandraCh <saichandrachilupuri@gmail.com>
2018-12-11 16:01:04 +0530

    problem 896 solved

---
+++ src/Problem_485.java
@@ -0,0 +1,9 @@
+public class Problem_485 {
+    public static void main(String[] args) {
+        findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1});
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+
+        return 0;
+    }
+}
---
+++ src/Problem_566.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_566 {
+    public static void main(String[] args) {
+        matrixReshape(new int[][]{{1,2},{3,4}},1,4);
+    }
+
+    private static int[][] matrixReshape(int[][] nums, int r, int c) {
+        int row=nums.length,col=nums[0].length,index = 0 ;
+        List values = new ArrayList();
+        int[][] matrix = new int[r][c];
+        if (r*c == row*col){
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    values.add(nums[i][j]);
+                }
+            }
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++) {
+                    matrix[i][j]= (int) values.get(index);
+                    index++;
+                }
+            }
+            return matrix;
+        }else{
+            return nums;
+        }
+    }
+}
