2a8dcc3758f7fe46e572ac7ceda403be34b71861
SaiChandraCh <saichandrachilupuri@gmail.com>
2018-12-14 20:56:33 +0530

    Brute force method

--- src/Problem_169.java
+++ src/Problem_169.java
@@ -1,27 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Problem_169 {
     public static void main(String[] args) {
-        majorityElement(new int[]{2,2,1,1,1,2,2,});
+        majorityElement(new int[]{2,2,1,1,1,2,2});
     }
     public static int majorityElement(int[] nums) {
-        int maxNum = nums[0],count=0,tempCount=0;
+        int maxNum = nums[0],count=0,tempCount=0,temp;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if(nums[i]==nums[j]){
+            for (int j = i+1; j < nums.length; j++) {
+                temp=nums[i];
+                if(temp==nums[j]){
                     tempCount++;
                     if(tempCount>count){
                         count=tempCount;
-                        maxNum=nums[i];
+                        maxNum=temp;
                     }
                 }
             }
             tempCount=0;
         }
+        System.out.println(maxNum);
         return maxNum;
     }
 }
